6fb5ffbfdb88f17221c866f8fea61a6511dc6543
Ubuntu <adminfabmedical@fabmedical-d01.clzxyz3roz5uvkwdwmtx5pdjsd.ax.internal.cloudapp.net>
2021-09-22 10:11:45 +0000

    add connection string

--- content-api/config/config.js
+++ content-api/config/config.js
@@ -1,5 +1,6 @@
 exports.appSettings = {
     db: (function(){
-        return process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/contentdb';
+        return 'mongodb://fabmedical-d01:GtuNuer7d2YhKy6qk9fNpocopsoroBqgmLzD564G4jiwIeov6JSny6zCH6Ze70vuq3MdpLYBMg8FueWulv5GYQ==@fabmedical-d01.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@fabmedical-d01@';
+        //process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/contentdb';
     })()
 };
